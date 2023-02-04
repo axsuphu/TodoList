794d8e707268dbe68e583f6db7c1459179daf5ab
Alyssa Suphunthuchat <axsuphu@gmail.com>
2023-02-03 17:35:12 -0800

    add complete functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { useState } from "react";
-import NewTask from "./NewTask";
+import { NewTask } from "./NewTask";
 
 function App() {
   //todo item will be an object that has id and task name
@@ -18,6 +18,7 @@ function App() {
       // if there is nothing in array, id:1 other wise, id:last-element-plus-1
       id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
       taskName: newTask,
+      completed: false,
     };
     //define new variable to include whatever is already inside todoList with the newTask added
     setTodoList([...todoList, task]);
@@ -33,6 +34,21 @@ function App() {
     setTodoList(todoList.filter((task) => (task.id !== id ? true : false)));
   };
 
+  const handleComplete = (id) => {
+    // change the completed key to the value of false when complete button is clicked
+    setTodoList(
+      todoList.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: true };
+        } else {
+          return task;
+        }
+      })
+    );
+  };
+
+  console.log(todoList);
+
   return (
     <div className="App">
       <div className="create-task">
@@ -44,15 +60,16 @@ function App() {
       <div>
         <h1>Tasks</h1>
         <ul>
-          {todoList.map((task) => {
-            return (
+          {todoList.map((task) =>
+            task.completed === false ? (
               <NewTask
                 taskId={task.id}
                 taskName={task.taskName}
                 handleDelete={handleDelete}
+                handleComplete={handleComplete}
               />
-            );
-          })}
+            ) : null
+          )}
         </ul>
       </div>
     </div>
--- src/NewTask.js
+++ src/NewTask.js
@@ -1,11 +1,10 @@
-function NewTask({ taskId, taskName, handleDelete }) {
+// normally, parameter passed in is (props). But instead of writing props.taskId, props is destructured and the name passed down can be used without dot notation
+export const NewTask = ({ taskId, taskName, handleDelete, handleComplete }) => {
   return (
     <div>
       <li key={taskId}>{taskName}</li>
-      <button>Edit</button>
+      <button onClick={() => handleComplete(taskId)}>Complete</button>
       <button onClick={() => handleDelete(taskId)}>Delete</button>
     </div>
   );
-}
-
-export default NewTask;
+};
